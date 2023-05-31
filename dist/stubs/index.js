535fc823b5b4cd876ebcb66777305f9c62054c81
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-05-31 15:47:59 +0000

    c2 c6 support

--- dist/stubs/index.js
+++ dist/stubs/index.js
@@ -1,4 +1,4 @@
-import { CHIP_FAMILY_ESP32, CHIP_FAMILY_ESP32S2, CHIP_FAMILY_ESP32S3, CHIP_FAMILY_ESP8266, CHIP_FAMILY_ESP32C3 } from "../const";
+import { CHIP_FAMILY_ESP32, CHIP_FAMILY_ESP32S2, CHIP_FAMILY_ESP32S3, CHIP_FAMILY_ESP8266, CHIP_FAMILY_ESP32C2, CHIP_FAMILY_ESP32C3, CHIP_FAMILY_ESP32C6, CHIP_FAMILY_ESP32H2 } from "../const";
 import { toByteArray } from "../util";
 export const getStubCode = async (chipFamily) => {
     let stubcode;
@@ -14,8 +14,17 @@ export const getStubCode = async (chipFamily) => {
     else if (chipFamily == CHIP_FAMILY_ESP8266) {
         stubcode = await import("./esp8266.json");
     }
+    else if (chipFamily == CHIP_FAMILY_ESP32C2) {
+        stubcode = await import("./esp32c2.json");
+    }
     else if (chipFamily == CHIP_FAMILY_ESP32C3) {
         stubcode = await import("./esp32c3.json");
+    }
+    else if (chipFamily == CHIP_FAMILY_ESP32C6) {
+        stubcode = await import("./esp32c6.json");
+    }
+    else if (chipFamily == CHIP_FAMILY_ESP32H2) {
+        stubcode = await import("./esp32h2.json");
     }
     // Base64 decode the text and data
     return {
